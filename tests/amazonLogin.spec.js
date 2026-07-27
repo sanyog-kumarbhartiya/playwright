13b9ad01dd4eae1f9c6c77e732dc1886b4a1a396
sanyog-kumarbhartiya <sanyogkumarbhartiya@gmail.com>
2026-07-27 21:02:40 +0530

    Initial commit

---
+++ tests/amazonLogin.spec.js
@@ -0,0 +1,30 @@
+const { test, expect } = require('@playwright/test');
+
+test('Amazon Login - Enter Email and Continue', async ({ page }) => {
+
+    // Open Amazon India
+    await page.goto('https://www.amazon.in');
+
+    await page.locator('#nav-link-accountList').click();
+
+    // Navigate to Sign-In page
+    await page.locator('#nav-link-accountList-nav-line-1');
+
+    // Wait for the email input
+    await page.waitForSelector('#ap_email_login');
+
+    // Click the email field
+    await page.locator('#ap_email_login').click();
+
+    // Enter the email
+    await page.locator('#ap_email_login').fill('sanyogkumarbhartiya@gmail.com');
+
+    // Click Continue
+    await page.locator('#continue').click();
+
+    // Optional: wait to observe the result
+    await page.waitForTimeout(5000);
+
+    await page.locator('#ap_password').click();
+    await page.locator('#ap_password').fill('Sanyog@9682446689');
+});
