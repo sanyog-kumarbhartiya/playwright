0ff185f21f4ce35676bf63a9eec9431906f48d6b
sanyog-kumarbhartiya <sanyogkumarbhartiya@gmail.com>
2026-07-29 23:16:37 +0530

    add junit report

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -9,27 +9,59 @@ const DetailProductPage = require('../pages/DetailProductPage');
 
 test('Amazon Login and Search', async ({ page }) => {
 
+    let result;
+
     const login = new LoginPage(page);
     const search = new SearchPage(page);
     const product = new ProductPage(page);
-    
 
-    await login.openAmazon();
+    await test.step('Step 1: Open Amazon and Login', async () => {
 
-    await login.clickSignIn();
+        console.log("========== Step 1 ==========");
+        console.log("Login to Amazon");
 
-    await login.login(
-        process.env.AMAZON_EMAIL,
-        process.env.AMAZON_PASSWORD
-    );
+        await login.openAmazon();
+        await login.clickSignIn();
 
-    await search.searchProduct("watch");
-    await page.waitForTimeout(5000);
+        await login.login(
+            process.env.AMAZON_EMAIL,
+            process.env.AMAZON_PASSWORD
+        );
 
-    const result = await product.getFirstProductDetails();
+        console.log("Login Successful");
+    });
 
-const details = new DetailProductPage(result.newPage);
+    await test.step('Step 2: Search Product', async () => {
 
-await details.ProductDetails();
+        console.log("========== Step 2 ==========");
+        console.log("Searching Product : watch");
+
+        await search.searchProduct("watch");
+
+        console.log("Search Completed");
+    });
+
+    await test.step('Step 3: Open First Product', async () => {
+
+        console.log("========== Step 3 ==========");
+
+        result = await product.getFirstProductDetails();
+
+        console.log("First Product Opened");
+    });
+
+    await test.step('Step 4: Verify Product Details', async () => {
+
+        console.log("========== Step 4 ==========");
+
+        const details = new DetailProductPage(result.newPage);
+
+        const productDetails = await details.ProductDetails();
+
+        // console.log("Product Name :", productDetails.name);
+        // console.log("Discount    :", productDetails.discount);
+        // console.log("Price       :", productDetails.price);
+        // console.log("MRP         :", productDetails.mrp);
+    });
 
 });
